55320b0b068f5c75a683a1f963169697757be436
kosta <kostabere@gmail.com>
2023-06-14 14:23:01 +0300

    new remap

--- lua/main_folder/remap.lua
+++ lua/main_folder/remap.lua
@@ -13,6 +13,9 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
+vim.keymap.set("n", "H", "gT")
+vim.keymap.set("n", "L", "gt")
+
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
 -- next greatest remap ever : asbjornHaland
@@ -33,10 +36,11 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
---if vim.fn.has('unix') == 0 then
---    vim.keymap.set("n", "<leader>cfg", "<cmd>e ~/AppData/Local/nvim/init.lua<CR>");
---else
---    vim.keymap.set("n", "<leader>cfg", "<cmd>e ~/.config/nvim/init.lua<CR>");
+if vim.fn.has('unix') == 0 then
+    vim.keymap.set("n", "<leader>cfg", "<cmd>e ~/AppData/Local/nvim/<CR>");
+else
+    vim.keymap.set("n", "<leader>cfg", "<cmd>e ~/.config/nvim/<CR>");
+end
 
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
